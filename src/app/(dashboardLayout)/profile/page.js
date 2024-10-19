41c623b3acf89762a7a362a8e168b0f8a2427f0d
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-19 11:46:58 +0600

    change commit

--- src/app/(dashboardLayout)/profile/page.js
+++ src/app/(dashboardLayout)/profile/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const ProfilePage = () => {
   return (
     <div>
-      <h2>Profile page sdfjdskfsdfjsdfsdfjdkfjsdkfjsd</h2>
+      <h2>Profile page my profile</h2>
     </div>
   );
 };
